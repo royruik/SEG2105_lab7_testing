4ff9247d792c9cc48fb7e6b28c237ce360cd5d7f
Roy Rui <yrui053@uottawa.ca>
2023-11-13 20:13:11 -0500

    Finished Lab7

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     EditText userName;
@@ -40,4 +43,16 @@ public class LoginActivity extends AppCompatActivity {
         else
             return "Invalid login!";
     }
+    public String validate_email(String userName) {
+        String regex = "^[a-zA-Z0-9_][a-zA-Z0-9_]+@[a-zA-Z0-9_]+(?:\\.[a-zA-Z0-9]+)";
+        Pattern pattern = Pattern.compile(regex, Pattern.UNICODE_CASE);
+        Matcher matcher = pattern.matcher(userName);
+
+        if (matcher.matches()) {
+            return "Valid Username";
+        } else {
+            return "Invalid Username!";
+        }
+    }
+
 }
--- app/src/test/java/com/example/myapplication/LoginUnitTest.java
+++ app/src/test/java/com/example/myapplication/LoginUnitTest.java
@@ -16,6 +16,8 @@ public class LoginUnitTest {
 
     private static final String FAKE_STRING = "Login was successful";
 
+    private static final String FAKE_USERNAME_STRING="Valid Username";
+
     @Mock
     Context mMockContext;
 
@@ -29,5 +31,27 @@ public class LoginUnitTest {
 
         // ...then the result should be the expected one.
         assertThat(result, is(FAKE_STRING));
+    }
+
+    @Test
+    public void testValidUsername() {
+        LoginActivity myObjectUnderTest = new LoginActivity(mMockContext);
+
+        // Checking for valid username.
+        String result = myObjectUnderTest.validate_email("admin123@uottawa.ca");
+
+        // Both should match!
+        assertThat(result, is(FAKE_USERNAME_STRING));
+    }
+
+    @Test
+    public void testInvalidUsername() {
+        LoginActivity myObjectUnderTest = new LoginActivity(mMockContext);
+
+        // Checking for invalid username.
+        String result = myObjectUnderTest.validate_email("!!!!@g@1$.com");
+
+        // Both should not be equal, hence error when testing.
+        assertThat(result, is(FAKE_USERNAME_STRING));
     }
 }
